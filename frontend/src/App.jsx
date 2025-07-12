b31ac7ac1f75334e490c0fe75df4c176afab47a4
Muhammad Abubakar <m3abubakar@gmail.com>
2025-07-12 15:40:38 +0500

    creating frontend layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+// import './App.css'
+
+import AllLinks from "./components/AllLinks"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-screen bg-gray-100">
+        <Navbar/>
+        <AllLinks/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/AllLinks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllLinks = () => {
+  return (
+    <div className='bg-white max-w-5xl mx-auto mt-36 shadow-md p-4 rounded-lg'>AllLinks</div>
+  )
+}
+
+export default AllLinks
